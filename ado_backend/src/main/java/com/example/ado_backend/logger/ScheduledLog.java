89c1fa5983b42456d3023f4f8447adde19637c7d
Anaelle <anaellejeagni@gmail.com>
2022-04-18 20:35:36 +0200

    Implement a logger to print hour each 5 seconds

---
+++ ado_backend/src/main/java/com/example/ado_backend/logger/ScheduledLog.java
@@ -0,0 +1,21 @@
+package com.example.ado_backend.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@EnableScheduling
+public class ScheduledLog {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledLog.class);
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime(){
+        LOGGER.info("The current time now is {}", LocalDateTime.now());
+    }
+}
